7db84ec0e8650c585eb773cb101126db52f0630a
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-26 16:10:26 -0400

    used the state value items in the Menu component; in the return, create a map() method which has a destructured props; its return will have the structured that will show the UI on the page

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((food) => {
+        const { id, title, category, price, img, desc } = food;
+
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <h4>{title}</h4>
+              <p className="price">{price}</p>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
