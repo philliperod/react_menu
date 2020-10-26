b4822b8036ced565248f0dd7fc8c2e1b5f6ad330
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-26 15:21:47 -0400

    created two useState hooks - one for each prop in the data object and one specific for the categories prop; structured jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>Menu</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [category, setCategory] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
+
+// 1) need to create two useState hooks - one for each properties of the data object and one for the property categories
+// the default value for the first hook will be the props from the data object
+// the default value for the second hook will be an empty array
+// 2) structured your jsx
